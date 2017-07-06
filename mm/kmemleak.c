42c874f01141ed5a395ddbcefd3895842534a028
Catalin Marinas <catalin.marinas@arm.com>
2017-07-06 15:40:19 -0700

    mm: kmemleak: factor object reference updating out of scan_block()
    scan_block() updates the number of references (pointers) to objects,
    adding them to the gray_list when object->min_count is reached.  The
    patch factors out this functionality into a separate update_refs()
    function.
    
    Link: http://lkml.kernel.org/r/1495726937-23557-3-git-send-email-catalin.marinas@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: "Luis R. Rodriguez" <mcgrof@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1135,6 +1135,28 @@ static bool update_checksum(struct kmemleak_object *object)
 }
 
 /*
+ * Update an object's references. object->lock must be held by the caller.
+ */
+static void update_refs(struct kmemleak_object *object)
+{
+	if (!color_white(object)) {
+		/* non-orphan, ignored or new */
+		return
+	}
+
+	/*
+	 * Increase the object's reference count (number of pointers
+	 * to the memory block). If this count reaches the required
+	 * minimum, the object's color will become gray and it will be
+	 * added to the gray_list.
+	 */
+	object->count++;
+	if (color_gray(object)) {
+		list_add_tail(&object->gray_list, &gray_list);
+	}
+}
+
+/*
  * Memory scanning is a long process and it needs to be interruptable. This
  * function checks whether such interrupt condition occurred.
  */
@@ -1201,26 +1223,7 @@ static void scan_block(void *_start, void *_end,
 		 */
 		spin_lock_irqsave_nested(&object->lock, flags,
 					 SINGLE_DEPTH_NESTING);
-		if (!color_white(object)) {
-			/* non-orphan, ignored or new */
-			spin_unlock_irqrestore(&object->lock, flags);
-			put_object(object);
-			continue;
-		}
-
-		/*
-		 * Increase the object's reference count (number of pointers
-		 * to the memory block). If this count reaches the required
-		 * minimum, the object's color will become gray and it will be
-		 * added to the gray_list.
-		 */
-		object->count++;
-		if (color_gray(object)) {
-			list_add_tail(&object->gray_list, &gray_list);
-			spin_unlock_irqrestore(&object->lock, flags);
-			continue;
-		}
-
+		update_refs(object);
 		spin_unlock_irqrestore(&object->lock, flags);
 		put_object(object);
 	}
