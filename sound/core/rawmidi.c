7633823485da8517fb5a8cbe153ad29a86c98565
Daniel Rosenberg <drosen@google.com>
2017-10-31 16:55:26 -0700

    ANDROID: sound: rawmidi: Hold lock around realloc
    The SNDRV_RAWMIDI_STREAM_{OUTPUT,INPUT} ioctls may reallocate
    runtime->buffer while other kernel threads are accessing it.  If the
    underlying krealloc() call frees the original buffer, then this can turn
    into a use-after-free.
    
    Most of these accesses happen while the thread is holding runtime->lock,
    and can be fixed by just holding the same lock while replacing
    runtime->buffer, however we can't hold this spinlock while
    snd_rawmidi_kernel_{read1,write1} are copying to/from userspace.  We
    need to add and acquire a new mutex to prevent this from happening
    concurrently with reallocation.  We hold this mutex during the entire
    reallocation process, to also prevent multiple concurrent reallocations
    leading to a double-free.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    bug: 64315347
    Change-Id: I05764d4f1a38f373eb7c0ac1c98607ee5ff0eded
    [dcagle@codeaurora.org: Resolve trivial merge conflict]
    Git-repo: https://android.googlesource.com/kernel/msm
    Git-commit: d7193540482d11ff0ad3a07fc18717811641c6eb
    Signed-off-by: Dennis Cagle <dcagle@codeaurora.org>

--- sound/core/rawmidi.c
+++ sound/core/rawmidi.c
@@ -981,6 +981,8 @@ static long snd_rawmidi_kernel_read1(struct snd_rawmidi_substream *substream,
 		mutex_lock(&runtime->realloc_mutex);
 
 	spin_lock_irqsave(&runtime->lock, flags);
+	if (userbuf)
+		mutex_lock(&runtime->realloc_mutex);
 	while (count > 0 && runtime->avail) {
 		count1 = runtime->buffer_size - runtime->appl_ptr;
 		if (count1 > count)
@@ -1009,7 +1011,6 @@ static long snd_rawmidi_kernel_read1(struct snd_rawmidi_substream *substream,
 		count -= count1;
 	}
 	spin_unlock_irqrestore(&runtime->lock, flags);
-
 	if (userbuf)
 		mutex_unlock(&runtime->realloc_mutex);
 	return result;
