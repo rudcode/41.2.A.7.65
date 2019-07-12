2d2082b9d5d806380b98dc1ce9866d588a494170
Geert Uytterhoeven <geert+renesas@glider.be>
2019-07-11 20:59:06 -0700

    mm/vmalloc.c: spelling> s/informaion/information/
    Link: http://lkml.kernel.org/r/20190607113509.15032-1-geert+renesas@glider.be
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2791,7 +2791,7 @@ static int aligned_vwrite(char *buf, char *addr, unsigned long count)
  *	Note: In usual ops, vread() is never necessary because the caller
  *	should know vmalloc() area is valid and can use memcpy().
  *	This is for routines which have to access vmalloc area without
- *	any informaion, as /dev/kmem.
+ *	any information, as /dev/kmem.
  *
  */
 
@@ -2873,7 +2873,7 @@ finished:
  *	Note: In usual ops, vwrite() is never necessary because the caller
  *	should know vmalloc() area is valid and can use memcpy().
  *	This is for routines which have to access vmalloc area without
- *	any informaion, as /dev/kmem.
+ *	any information, as /dev/kmem.
  */
 
 long vwrite(char *buf, char *addr, unsigned long count)
